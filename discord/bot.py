c7c1a22411006bdf7fb2cf362b83f6dc1c6cdab0
mkavanagh7 <142540381+mkavanagh7@users.noreply.github.com>
2023-10-26 13:41:46 +0100

    Add files via upload

--- discord/bot.py
+++ discord/bot.py
@@ -3,22 +3,20 @@ import responses
 
 async def send_message(message, user_message, is_private):
     try:
-        response = responses.handle_response(user_message)
+        response = responses.get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
         
 def run_discord_bot():
-    TOKEN = 'MTE2NjA5NzA2Mzg3NzAzODA5MA.GOrbdP.eCtvGOLra4qL_OiBi73SxGXo8frWvQSQs5AiVQ'
-    # client = discord.Client(intents=discord.Intents.default())
-    intents = discord.Intents(messages=True, guilds=True)
-    # intents = discord.Intents.default()
-    # intents.message_content = True
-    # client = discord.Client(intents=intents)
-    # intents = discord.Intents.default()
-    intents.members = True
+    TOKEN = 'MTE2NjA5NzA2Mzg3NzAzODA5MA.GIEbOe.U7Z9n6q2wrcFJJEgcL12e9LHeyiOgYKj-g6Zks'
+    intents = discord.Intents.default()
     intents.message_content = True
+    #intents = discord.Intents(messages=True, guilds=True)
+    intents.members = True
     client = discord.Client(intents=intents)
+    #intents.messages.content = True
+    
 
     @client.event
     async def on_ready():
@@ -32,7 +30,15 @@ def run_discord_bot():
         user_message=str(message.content)
         channel=str(message.channel)
         
-        print(f"{username}"+" said: "+{user_message}+" ({channel})" )
+        print(f'{username} said: "{user_message}" ({channel})')
+        
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:  
+            await send_message(message, user_message, is_private=False)
+
+            
         
     client.run(TOKEN)
     
