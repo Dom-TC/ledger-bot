af138a3f060fc20f45596e4dfae8d4472e4ef851
Dom Chester <dom.chester@me.com>
2023-10-28 13:20:46 +0100

    Initial slash command setup / test

--- ledger_bot/LedgerBot.py
+++ ledger_bot/LedgerBot.py
@@ -3,21 +3,43 @@
 import logging
 
 import discord
+from discord import app_commands
 
 log = logging.getLogger(__name__)
 
 
 class LedgerBot(discord.Client):
+    """
+    The main bot class.
+
+    Paramaters
+    ----------
+    config : dict
+        The bots config options
+
+    storage : storage.AirtableStorage
+        The AirTable storage
+
+    guild : discord.Object
+        The Discord Server the bot is running in
+
+    """
+
     def __init__(self, config: dict, storage):
         self.config = config
         self.storage = storage
+        self.guild = discord.Object(id=self.config["guild"])
 
-        log.debug("Watching channels: %s", self.config["channels"])
+        log.info(f"Set guild: {self.config['guild']}")
+        log.info(f"Watching channels: {self.config['channels']}")
 
         intents = discord.Intents(
             messages=True, guilds=True, reactions=True, message_content=True
         )
+
         super().__init__(intents=intents)
+
+        self.tree = app_commands.CommandTree(self)
 
     async def on_ready(self):
         log.info(f"We have logged in as {self.user}")
@@ -28,6 +50,9 @@ class LedgerBot(discord.Client):
                 name=self.config["watching_status"],
             )
         )
+
+        log.info("Building slash commands")
+        await self.tree.sync(guild=self.guild)
 
     async def on_message(self, message):
         channel_name = message.channel.name
