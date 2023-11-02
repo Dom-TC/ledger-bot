2bfa7296384be2e191e2b6ba2a68b0fbfb27977d
Dom Chester <dom.chester@me.com>
2023-11-03 01:02:26 +0300

    Refactored into `approve_transaction()`

--- ledger_bot/LedgerBot.py
+++ ledger_bot/LedgerBot.py
@@ -8,6 +8,7 @@ from discord import app_commands
 from message_generator import generate_transaction_status_message
 from models import AirTableError
 from process_dm import is_dm, process_dm
+from process_transactions import approve_transaction
 
 log = logging.getLogger(__name__)
 
@@ -141,68 +142,16 @@ class LedgerBot(discord.Client):
 
         if payload.emoji.name == self.config["emojis"]["approval"]:
             # Approval
-            if reactor.id != buyer.id:
-                log.info(
-                    f"Ignoring {payload.emoji.name} from {reactor.name} on message {payload.message_id} in {channel.name} - Reactor is not the buyer"
-                )
-                return
-
-            if target_transaction.sale_approved:
-                log.info(
-                    f"Ignoring approval on transaction {target_transaction.row_id}. Transaction already approved"
-                )
-                return
-
-            log.info(f"Approving transaction {target_transaction.row_id}")
-
-            target_transaction.sale_approved = True
-            target_transaction.approved_date = datetime.datetime.utcnow().isoformat()
-            transaction_fields = ["sale_approved", "approved_date"]
-            await self.storage.save_transaction(
-                transaction=target_transaction, fields=transaction_fields
-            )
-
-            response_contents = generate_transaction_status_message(
-                seller=seller,
+            await approve_transaction(
+                reactor=reactor,
                 buyer=buyer,
-                wine_name=target_transaction.wine,
-                wine_price=target_transaction.price,
+                seller=seller,
+                payload=payload,
+                channel=channel,
+                target_transaction=target_transaction,
                 config=self.config,
-                is_update=True,
-                is_approved=True,
+                storage=self.storage,
             )
-
-            try:
-                sent_message = await channel.send(response_contents)
-                await self.storage.record_bot_message(
-                    message=sent_message, transaction=target_transaction
-                )
-
-            except discord.HTTPException as error:
-                log.error(f"An error occured sending the message: {error}")
-            except discord.Forbidden as error:
-                log.error(f"You don't have permission to send to that channel: {error}")
-            except AirTableError as error:
-                log.error(f"An error occured storing the content in AirTable: {error}")
-
-            if self.config["delete_previous_bot_messages"]:
-                log.info("delete_previous_bot_messages is true")
-                log.info(f"Removing bot_message {payload.message_id}")
-                old_message = await channel.fetch_message(payload.message_id)
-
-                try:
-                    await old_message.delete()
-                    await self.storage.delete_bot_message(
-                        await self.storage(payload.message_id)
-                    )
-                except discord.Forbidden as error:
-                    log.error(
-                        f"You don't have permission to send to that channel: {error}"
-                    )
-                except discord.NotFound as error:
-                    log.error(f"The message has already been deleted: {error}")
-                except discord.HTTPException as error:
-                    log.error(f"An error occured deleting the message: {error}")
 
     async def on_disconnect(self):
         log.warning("Bot disconnected")
