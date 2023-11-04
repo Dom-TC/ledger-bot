dcc1a6893faeb3943e5f34c3f8a073976e857c3a
Dom Chester <dom.chester@me.com>
2023-11-04 18:57:48 +0300

    Add help commands

--- ledger_bot/message_generators/__init__.py
+++ ledger_bot/message_generators/__init__.py
@@ -1,6 +1,7 @@
 """Functions for generating message content posted by ledger-bot."""
-from . import generate_transaction_status_message
+from . import generate_help_message, generate_transaction_status_message
 
 generate_transaction_status_message = (
     generate_transaction_status_message.generate_transaction_status_message
 )
+generate_help_message = generate_help_message.generate_help_message
--- ledger_bot/process_dm/__init__.py
+++ ledger_bot/process_dm/__init__.py
@@ -1,8 +1,9 @@
 """Functions to process DM messages."""
 
-from . import command_version, get_dm_channel, is_dm, process_dm
+from . import command_help, command_version, get_dm_channel, is_dm, process_dm
 
 get_dm_channel = get_dm_channel.get_dm_channel
 is_dm = is_dm.is_dm
 process_dm = process_dm.process_dm
 command_version = command_version.command_version
+command_help = command_help.command_help
