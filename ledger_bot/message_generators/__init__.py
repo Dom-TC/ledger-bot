93afb03d827a35424a7a057d1ab80f70f0e841b8
Dom Chester <dom.chester@me.com>
2023-11-07 01:14:56 +0300

    Add `!list`

--- ledger_bot/message_generators/__init__.py
+++ ledger_bot/message_generators/__init__.py
@@ -1,7 +1,12 @@
 """Functions for generating message content posted by ledger-bot."""
-from . import generate_help_message, generate_transaction_status_message
+from . import (
+    generate_help_message,
+    generate_list_message,
+    generate_transaction_status_message,
+)
 
 generate_transaction_status_message = (
     generate_transaction_status_message.generate_transaction_status_message
 )
 generate_help_message = generate_help_message.generate_help_message
+generate_list_message = generate_list_message.generate_list_message
---
+++ ledger_bot/process_dm/command_list.py
@@ -0,0 +1,35 @@
+"""DM command - list."""
+
+import logging
+from typing import TYPE_CHECKING
+
+import discord
+
+from ledger_bot.message_generators import generate_list_message
+from ledger_bot.models import Transaction
+
+if TYPE_CHECKING:
+    from ledger_bot.LedgerBot import LedgerBot
+
+log = logging.getLogger(__name__)
+
+
+async def command_list(
+    client: "LedgerBot", message: discord.Message, dm_channel: discord.DMChannel
+):
+    """DM command - list."""
+    log.info(f"Getting transactions for user {message.author.name}")
+
+    transactions = await client.storage.get_users_transaction(message.author.id)
+
+    if len(transactions) == 0:
+        dm_channel.send("You don't have any open transactions.")
+
+    for i, transaction in enumerate(transactions):
+        transactions[i] = Transaction.from_airtable(transaction)
+
+    response = await generate_list_message(
+        transactions=transactions, user_id=message.author.id, storage=client.storage
+    )
+
+    await dm_channel.send(response)
