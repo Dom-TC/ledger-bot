2aeedc9b0057d187ea2790d08a68dba428b568b4
Dom Chester <dom.chester@me.com>
2023-11-07 18:09:26 +0300

    Check slash commands are run in in allowed channel

--- ledger_bot/slash_commands/command_hello.py
+++ ledger_bot/slash_commands/command_hello.py
@@ -17,4 +17,28 @@ async def command_hello(
     interaction: discord.Interaction,
 ):
     """Says hello."""
+    channel_name = interaction.channel.name
+
+    if (
+        config["channels"].get("include")
+        and channel_name not in config["channels"]["include"]
+    ):
+        log.info(
+            f"Ignoring slash command from {interaction.user.name} in {channel_name}  - Channel not in include list"
+        )
+        await interaction.response.send_message(
+            content=f"{config['name']} is not available in this channel.",
+            ephemeral=True,
+        )
+        return
+    elif channel_name in config["channels"].get("exclude", []):
+        log.info(
+            f"Ignoring slash command from {interaction.user.name} in {channel_name}  - Channel in exclude list"
+        )
+        await interaction.response.send_message(
+            content=f"{config['name']} is not available in this channel.",
+            ephemeral=True,
+        )
+        return
+
     await interaction.response.send_message(f"Hi, {interaction.user.mention}")
