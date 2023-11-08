8c763e0ab59aca75b01ba49e838ea99478e5355b
Dom Chester <dom.chester@me.com>
2023-11-08 17:56:15 +0300

    Reduce discordpy logging level

--- ledger_bot/__main__.py
+++ ledger_bot/__main__.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 # Configure logging
 logging.config.fileConfig(fname="log.conf", disable_existing_loggers=False)
-logging.getLogger("discord").setLevel(logging.CRITICAL)
+logging.getLogger("discord").setLevel(logging.ERROR)
 logging.getLogger("discord.gateway").setLevel(logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("urllib").setLevel(logging.CRITICAL)
