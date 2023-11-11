1562729d915a08720cd5a05c943738c4e524967f
Dom Chester <dom.chester@me.com>
2023-11-11 14:02:40 +0300

    Persist logs between deployments

--- ledger_bot/__main__.py
+++ ledger_bot/__main__.py
@@ -18,6 +18,7 @@ logging.getLogger("discord.gateway").setLevel(logging.INFO)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("urllib").setLevel(logging.CRITICAL)
 logging.getLogger("apscheduler.scheduler").setLevel(logging.INFO)
+
 if os.getenv("LOG_TO_FILE") == "false":
     logging.info("LOG_TO_FILE is false, removing FileHandlers")
     file_handlers = (
@@ -27,6 +28,19 @@ if os.getenv("LOG_TO_FILE") == "false":
     )
     for handler in file_handlers:
         logging.root.removeHandler(handler)
+elif os.getenv("LOG_FOLDER_PATH") is not None:
+    logging.info(f"Updating logging FileHandler path to {os.getenv('LOG_FOLDER_PATH')}")
+
+    file_handlers = (
+        handler
+        for handler in logging.root.handlers
+        if isinstance(handler, logging.FileHandler)
+    )
+    for handler in file_handlers:
+        handler.stream.close()
+        handler.baseFilename = os.path.abspath(f"{os.getenv('LOG_FOLDER_PATH')}/log")
+        handler.stream = handler._open()
+
 log = logging.getLogger(__name__)
 
 log.info("Starting ledger-bot")
