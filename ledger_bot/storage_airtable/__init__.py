f086bcbebe4353d29071667d05f1abfc062eb9d8
Dom Chester <dom.chester@me.com>
2025-08-26 13:18:21 +0100

    Remove airtable storage

--- ledger_bot/storage_airtable/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module for interacting with the AirTable database."""
-
-from .airtable_storage import AirtableStorage
-from .reaction_roles_storage import ReactionRolesStorage
-
-__all__ = ["AirtableStorage", "ReactionRolesStorage"]
