ef30cd5256197cbb70da0e5ed0e31a0f147469f7
Dom Chester <dom.chester@me.com>
2025-08-27 01:44:39 +0100

    Fix list commands

--- ledger_bot/commands_dm/command_list.py
+++ ledger_bot/commands_dm/command_list.py
@@ -22,12 +22,19 @@ async def command_list(
     log.info(f"Getting transactions for user {message.author.name}")
 
     try:
-        member = await client.service.member.get_or_add_member(message.author)
+        async with client.session_factory() as session:
+            member = await client.service.member.get_or_add_member(
+                message.author, session=session
+            )
 
-        buy_transactions = member.buying_transactions
-        sell_transactions = member.selling_transactions
+            await session.refresh(
+                member, attribute_names=["buying_transactions", "selling_transactions"]
+            )
 
-        transactions = buy_transactions + sell_transactions
+            buy_transactions = member.buying_transactions
+            sell_transactions = member.selling_transactions
+
+            transactions = buy_transactions + sell_transactions
     except AirTableError as error:
         log.error(f"There was an error processing the AirTable request: {error}")
         await dm_channel.send("An unexpected error occured.")
--- ledger_bot/commands_slash/command_list.py
+++ ledger_bot/commands_slash/command_list.py
@@ -1,4 +1,4 @@
-"""DM command - list."""
+"""Slash command - list."""
 
 import logging
 from typing import TYPE_CHECKING, Any
@@ -28,11 +28,19 @@ async def command_list(
     await interaction.response.defer(ephemeral=True)
 
     try:
-        member = await client.service.member.get_or_add_member(interaction.user)
-        buy_transactions = member.buying_transactions
-        sell_transactions = member.selling_transactions
+        async with client.session_factory() as session:
+            member = await client.service.member.get_or_add_member(
+                interaction.user, session=session
+            )
 
-        transactions = buy_transactions + sell_transactions
+            await session.refresh(
+                member, attribute_names=["buying_transactions", "selling_transactions"]
+            )
+
+            buy_transactions = member.buying_transactions
+            sell_transactions = member.selling_transactions
+
+            transactions = buy_transactions + sell_transactions
     except AirTableError as error:
         log.error(f"There was an error processing the AirTable request: {error}")
         await interaction.response.send_message(
